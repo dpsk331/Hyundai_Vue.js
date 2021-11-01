e03c8cae9347475d51824384efc0afa1f7de3cd3
dpsk331 <dpsk331@naver.com>
2021-11-01 17:04:53 +0900

    2021-11-01: 상태관리 > vuex 설치

--- front-end-vue/src/main.js
+++ front-end-vue/src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App) // index.html의 id가 app인 위치에 App 컴포넌트를 넣어줌
+  store,
+
+  // index.html의 id가 app인 위치에 App 컴포넌트를 넣어줌
+  render: h => h(App)
 }).$mount('#app') //  #app은 index.html에 id가 app인 태그를 가르킴
  
