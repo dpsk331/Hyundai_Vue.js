83d58ac5a3be6e952ca379e3d94bb465436e78da
dpsk331 <dpsk331@naver.com>
2021-11-01 15:51:53 +0900

    2021-11-01: 컴포넌트간 데이터 전달

--- front-end-vue/src/views/menu04/Exam01Props/index.vue
+++ front-end-vue/src/views/menu04/Exam01Props/index.vue
@@ -8,14 +8,33 @@
       <child-a :prop1="data1"
                :prop2="data2"
                :productNo="pno"
-               product-kind="bag"
+               :product-kind="pkind"
                :product-price="1000"/>
+      <hr/>
+      <child-b :product="product" v-bind="product"/>
+
+      <!-- 아래 두 태그는 같은 효과 -->
+      <!-- 
+      <child-b v-bind="product"/>
+      <child-b v-bind:no="product.no" v-bind:kind="product.kind"/> -->
+      <hr />
+
+      <child-c propD="string-data" 
+               :propA="3"
+               :propB="2"
+               :propC="7"
+               :propE="9"
+               :propF="{message: 'hello, vue'}"
+               :propG="['red', 'green', 'blue']"
+               :propI="5" />
     </div>
   </div>
 </template>
 
 <script>
 import ChildA from"./ChildA.vue"
+import ChildB from"./ChildB.vue"
+import ChildC from './ChildC.vue'
 
 export default {
   //컴포넌트의 대표 이름(devtools에 나오는 이름)
@@ -23,13 +42,21 @@ export default {
   //추가하고 싶은 컴포넌트들 목록
   components:{
     ChildA,
+    ChildB,
+    ChildC
   },
   //컴포넌트 데이터 정의
   data: function() {
     return {
       data1: "value1",
       data2: "value2",
-      pno: "1"
+      pno: "1",
+      pkind: "bag",
+      pprice: 1000,
+      product: {
+        no: 2,
+        kind: "정장"
+      }
     };
   },
   //컴포넌트 메소드 정의
--- front-end-vue/src/views/menu04/Exam04Ref/Child.vue
+++ front-end-vue/src/views/menu04/Exam04Ref/Child.vue
@@ -2,10 +2,14 @@
 <template>
   <div class="card">
     <div class="card-header">
-      SubBComponent
+      Child
     </div>
     <div class="card-body">
+      <p>data: {{data}}</p>
 
+      <hr/>
+      <button class="btn btn-info btn-sm" @click="handleParentData">부모 데이터 읽기</button>
+      <button class="btn btn-info btn-sm" @click="handleParentMethod">부모 메소드 호출</button>
     </div>
   </div>
 </template>
@@ -13,25 +17,33 @@
 <script>
 export default {
   //컴포넌트의 대표이름(devtools에 나오는 이름)
-  name:"SubBComponent",
+  name:"Child",
   //추가하고 싶은 컴포넌트 등록
   components: {
-
+    
   },
   //컴포넌트에서 사용하는 데이터 정의
-  data: function() { // () => {
+  data() { // () => {
     return {
-      
+      data: "child-data"
     };
   },
   //컴포넌트에서 사용하는 메소드 정의
   methods: {
-
+    childMethod() {
+      console.log("childMethod() 실행");
+    },
+    handleParentData() {
+      console.log(this.$parent.data);
+    },
+    handleParentMethod() {
+      console.log(this.$parent.parentMethod());
+    }
   }
 }
 </script>
 
 <!-- 컴포넌트 스타일 정의 -->
 <style scoped> /* scoped는 현재 template에만 적용한다는 의미 */
-
+  
 </style>
