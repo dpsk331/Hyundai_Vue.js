953b83cc1e6c4dadec2eca73e678b9f7f6a53dbc
dpsk331 <dpsk331@naver.com>
2021-11-02 17:18:03 +0900

    주석 오타 수정

--- front-end-vue/src/apis/axiosConfig.js
+++ front-end-vue/src/apis/axiosConfig.js
@@ -3,12 +3,12 @@ import axios from "axios";
 //기본 경로 설정
 axios.defaults.baseURL = 'http://localhost';
 
-//요청 HTTP에서 Authorization 헤어 추가, 값은 JWT로 설정
+//요청 HTTP에서 Authorization 헤더 추가, 값은 JWT로 설정
 function addAuthHeader(authToken) {
   axios.defaults.headers.common['Authorization'] = `Bearer ${authToken}`;
 }
 
-//요창 HTTP에 Authorization 헤더 제거
+//요청 HTTP에 Authorization 헤더 제거
 function removeAuthHeader() {
   delete axios.defaults.headers.common['Authorization'];
 }
