46a78c1ed422cab5e2a124638ea8a024c12993b6
dpsk331 <dpsk331@naver.com>
2021-11-04 15:53:52 +0900

    Vuetify: 개별 페이지 정의

--- front-end-vue-vuetify/src/views/About.vue
+++ front-end-vue-vuetify/src/views/About.vue
@@ -1,5 +1,42 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-card width="500" class="mx-auto my-2">
+    <v-img src="@/assets/photos/photo3.jpg" height="250" />
+    <v-card-title>About</v-card-title>
+    <v-card-subtitle>This is an about page</v-card-subtitle>
+    <v-divider class="mx-4"/>
+    <v-card-text>
+      The v-card component is a versatile component that can be used for anything from a panel to a static image. 
+    </v-card-text>
+    <v-divider class="mx-4"/>
+    <v-card-text>
+      The card component has numerous helper components to make markup as easy as possible. 
+    </v-card-text>
+    <v-card-actions>
+      <div class="mx-auto">
+        <v-btn class="mr-2" color="success" fab>OK</v-btn>
+        <v-btn color="info">Cancel</v-btn>
+      </div>
+    </v-card-actions>
+  </v-card>
 </template>
+
+<script>
+export default {
+  name:"About",
+  components: {
+    
+  },
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
