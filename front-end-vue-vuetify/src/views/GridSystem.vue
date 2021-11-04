16491ab3e549b1d036e8ee1af6b0afa9e0908839
dpsk331 <dpsk331@naver.com>
2021-11-04 17:52:48 +0900

    Vuetify: Grid system

---
+++ front-end-vue-vuetify/src/views/GridSystem.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card>
+    <v-card-title>Grid System</v-card-title>
+    <v-card-text>
+      <v-row>
+        <v-col class="red" cols="12" sm="2" md="6" lg="10">A</v-col>
+        <v-col class="green" cols="12" sm="10" md="6" lg="2">B</v-col>
+      </v-row>
+      <v-row class="indigo lighten-3">
+        <v-col cols="12" sm="2" md="6" lg="10">A</v-col>
+        <v-col cols="12" sm="10" md="6" lg="2">B</v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name:"GridSystem",
+  components: {
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
